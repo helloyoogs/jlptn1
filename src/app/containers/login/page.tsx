a4dd4591ec544b7ef600615b615fa42316ba236f
helloyoogs <helloyoogs@naver.com>
2023-12-15 17:15:04 +0900

    구글 간편 로그인만 사용으로 변경

--- src/app/containers/login/page.tsx
+++ src/app/containers/login/page.tsx
@@ -3,11 +3,10 @@ import { useSession, signIn, signOut } from "next-auth/react"
 
 function Login() {
     const { data: session } = useSession()
+    console.log(session)
     return (
         <div>
             <button onClick={() => signIn("google", { callbackUrl: "/" })} >구글 로그인</button>
-            <button onClick={() => signIn("kakao", { callbackUrl: "/" })} >카카오 로그인</button>
-            <button onClick={() => signIn("naver", { callbackUrl: "/" })} >네이버 로그인</button>
         </div>
 
     )
