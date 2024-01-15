2d2f19f1f8814cc8dd303405fccdeb6a2e49fdd5
helloyoogs <helloyoogs@naver.com>
2024-01-15 11:19:52 +0900

    vercel 배포 완료

--- src/app/containers/test/[id]/page.tsx
+++ src/app/containers/test/[id]/page.tsx
@@ -123,7 +123,7 @@ export default function Page() {
                                                     <Fragment key={optionIndex}>
                                                         <div className={'question_item'}>
                                                             <div
-                                                                className={`question_choice_box ${selectedAnswers[question.number] === optionIndex + 1 || userAnswerDataKeys.includes(optionIndex + 3) ? 'active' : ''}`}
+                                                                className={`question_choice_box ${selectedAnswers[question.number] === optionIndex + 3 || userAnswerDataKeys.includes(optionIndex + 3) ? 'active' : ''}`}
                                                                 onClick={() => handleAnswer(question.number, optionIndex + 3)}>
                                                                 <div className={"question_choice_number"}>
                                                                     {optionIndex + 3}</div>
