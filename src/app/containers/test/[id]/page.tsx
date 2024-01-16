4b841f34e6ac9bc69418796e67e2c712cb634724
helloyoogs <helloyoogs@naver.com>
2024-01-16 15:47:01 +0900

    임시저장,제출하기 버튼 클릭 시 user가 이미 푼 임시저장 데이터가 있으면 put 그게 아니면 post

--- src/app/containers/test/[id]/page.tsx
+++ src/app/containers/test/[id]/page.tsx
@@ -39,7 +39,7 @@ export default function Page() {
                     setUserAnswerData(res);
                 });
         }
-    }, []);
+    }, [userEmail]);
 
     const handleFindData = async () => {
         try {
